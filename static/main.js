7bac864f8379a3d70e21d043fc5b0d7bed56e39e
Hiten Sharma <sharmahiten3@gmail.com>
2017-07-16 22:00:58 +0530

    UnCSSed styles, minification and added some meta info

--- static/main.js
+++ static/main.js
@@ -4,6 +4,7 @@
     var readyList = [];
     var readyFired = false;
     var readyEventHandlersInstalled = false;
+
     function ready() {
         if (!readyFired) {
             readyFired = true;
@@ -13,17 +14,18 @@
             readyList = [];
         }
     }
+
     function readyStateChange() {
-        if ( document.readyState === "complete" ) {
+        if (document.readyState === "complete") {
             ready();
         }
     }
     baseObj[funcName] = function(callback, context) {
         if (readyFired) {
-            setTimeout(function() {callback(context);}, 1);
+            setTimeout(function() { callback(context); }, 1);
             return;
         } else {
-            readyList.push({fn: callback, ctx: context});
+            readyList.push({ fn: callback, ctx: context });
         }
         if (document.readyState === "complete") {
             setTimeout(ready, 1);
@@ -41,8 +43,35 @@
 })("docReady", window);
 
 
-var start = function(){
-  document.querySelector('.loader').style.display = 'none';
+var start = function() {
+    var link = document.createElement('link');
+    link.href = '/static/style.min.css';
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    document.getElementsByTagName('head')[0].appendChild(link);
+    var oncssloaded = function(callback) {
+        for (var i = 0; i < document.styleSheets.length; i++) {
+            if (document.styleSheets[i].href === link.href) {
+                return callback();
+            }
+        }
+        setTimeout(function() {
+            oncssloaded(callback);
+        }, 50);
+    };
+
+    function callback() {
+        if (link.addEventListener) {
+            link.removeEventListener("load", callback);
+        }
+        document.querySelector('.loader').style.display = "none";
+        document.querySelector('.container').style.display = "block";
+    }
+    if (link.addEventListener) {
+        link.addEventListener("load", callback);
+    }
+    link.oncssloaded = oncssloaded;
+    oncssloaded(callback);
 }
 
 docReady(start);
